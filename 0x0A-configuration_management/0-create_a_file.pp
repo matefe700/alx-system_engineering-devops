6135f478be86284f1a00d74230d48f1154a16f8d
matefe700 <74415899+matefe700@users.noreply.github.com>
2023-10-02 11:24:24 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-#creates a file in the tmp dir of the node
-
-file { 'holberton':
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-}
--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,7 +0,0 @@
-# install flask
-# package
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
-}
